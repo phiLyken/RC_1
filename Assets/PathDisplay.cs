82cc9813ec490e1ec4809d37bb91803e433ad2b4
philipp zupke <trash@bytestyles.net>
2016-03-26 13:53:50 +0100

    path display tool

---
+++ Assets/PathDisplay.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PathDisplay : MonoBehaviour {
+
+    List<Vector3> points;
+    public GameObject PointPrefab;
+    public GameObject[] testObjects;
+    LineRenderer Line;
+
+    public static PathDisplay MakePathDisplay()
+    {
+        GameObject prefab = Resources.Load("path_display") as GameObject;
+        GameObject go = Instantiate(prefab);
+        return go.GetComponent<PathDisplay>();
+    }
+   
+
+    void Start()
+    {
+        List<Vector3> l = new List<Vector3>();
+        foreach(GameObject go in testObjects)
+        {
+            l.Add(go.transform.position);
+        }
+
+        //UpdatePositions(l);
+    }
+
+    List<Vector3> TilesToVectorList(List<Tile> tiles)
+    {
+        List<Vector3> m_list = new List<Vector3>();
+        foreach (Tile tile in tiles) m_list.Add(tile.GetPosition());
+        return (m_list);
+    }
+
+    public void UpdatePositions(List<Tile> tlist)
+    {
+        UpdatePositions(TilesToVectorList(tlist));
+    }
+
+    public void UpdatePositions(List<Vector3> v3list)
+    {
+        Debug.Log(v3list.Count);
+        points = v3list;
+        if (Line == null) Line = GetComponent<LineRenderer>();
+
+        MyMath.DeleteChildren(gameObject);
+
+        Line.SetVertexCount(v3list.Count);
+        for (int i = 0; i < v3list.Count; i++)
+        {
+            (Instantiate(PointPrefab, v3list[i], Quaternion.identity) as GameObject).transform.parent = transform;
+            Line.SetPosition(i, v3list[i]);
+        }
+    }
+}
