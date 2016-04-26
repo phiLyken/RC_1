9693a15a0cf71e155f81f03ba8a3852b5d41b826
phil <philipp.zupke@wooga.net>
2016-04-26 21:42:56 +0200

    some groundwork for unit tools

---
+++ Assets/Editor/UnitConfigs.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class UnitConfig {
+
+	public UnitConfig(string id){
+		ID = id;
+	}
+	public string ID;
+}
+public class UnitConfigs : ScriptableObject {
+
+	[SerializeField]
+	private List<UnitConfig> configs;
+
+	void OnEnable(){
+		if(configs == null){
+			configs = new List<UnitConfig>();
+		}
+	}
+
+	public void Add(UnitConfig conf){
+		configs.Add(conf);
+	}
+
+	public void RemoveAt(int index){
+		configs.RemoveAt(index);
+	}
+
+	public int COUNT{
+		get { return configs.Count; }
+	}
+
+	public UnitConfig GetConfigAt(int index){
+		return configs[index];
+	}
+
+	public UnitConfig GetConfig(string id){
+		foreach(UnitConfig c in configs) if(c.ID == id) return c;
+
+		Debug.LogWarning("CONFIG NOT FOUND "+id);
+		return null;
+	}
+}
---
+++ Assets/Editor/UnitEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+
+public class UnitEditor : EditorWindow {
+	
+	private enum State
+	{
+		BLANK,
+		EDIT,
+		ADD
+	}
+
+	private State state;
+
+	private const string PATH = @"Assets/Units/unit_config.asset";
+	private UnitConfigs Units;
+
+	[MenuItem("Foo/UnitEditor %#w")]
+	public static void Init(){
+		UnitEditor window = EditorWindow.GetWindow<UnitEditor>();
+		window.minSize= new Vector2(800,400);
+		window.Show();
+	}
+
+	void OnEnable(){
+		if(Units == null) LoadUnitConfigs();
+
+		state = State.BLANK;
+	}
+	void OnGUI(){
+		EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+		if(GUILayout.Button("Add")){
+			Units.Add( new UnitConfig("Bla "+UnityEngine.Random.Range(0,100000)));
+		}
+
+		EditorGUILayout.EndHorizontal();
+	}
+
+	void LoadUnitConfigs(){
+		Units = (UnitConfigs) AssetDatabase.LoadAssetAtPath(PATH, typeof(UnitConfigs));
+
+		if(Units == null){
+			CreateUnitConfig();
+		}
+	}
+
+	void CreateUnitConfig(){
+		Units = ScriptableObject.CreateInstance<UnitConfigs>();
+
+		AssetDatabase.CreateAsset(Units, PATH);
+		AssetDatabase.SaveAssets();
+		AssetDatabase.Refresh();
+
+	}
+
+}
