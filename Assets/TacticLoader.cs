a08172a01444e852db07a20b8ae77c58294b697a
philipp zupke <trash@bytestyles.net>
2016-05-08 15:32:41 +0200

    UI now loading when the game starts

---
+++ Assets/TacticLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TacticLoader : MonoBehaviour {
+
+    void Awake()
+    {
+        if(GameObject.FindGameObjectWithTag("UI") == null) { 
+            Instantiate(Resources.Load("tactic_ui"));
+        }
+    }
+
+}
