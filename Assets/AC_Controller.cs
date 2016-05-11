6a0eca12dc775aa02d2ee811a3f9c6a58d499017
phil <philipp.zupke@wooga.net>
2016-05-11 19:30:06 +0200

    no message

--- Assets/AC_Controller.cs
+++ Assets/AC_Controller.cs
@@ -2,14 +2,101 @@
 using System.Collections;
 
 public class AC_Controller : MonoBehaviour {
+	
+	public IEnumerator CamInputControl(){
 
-	// Use this for initialization
-	void Start () {
-	
+		//		Debug.Log(" Cam Input Start");
+		inputEnabled = true;
+		startDragPos =  MyMath.GetInputPos();
+
+		while(inputEnabled && (Input.touchCount > 0 || Input.GetMouseButton(0) || Input.GetAxis("Mouse ScrollWheel") != 0) ){
+
+			Vector3 mousePos = MyMath.GetInputPos();			
+
+			if (Input.touchCount > 1 || Input.GetAxis("Mouse ScrollWheel") != 0){				
+				StartCoroutine(ListenToZoomInput());
+				yield break;
+			}
+
+			if((startDragPos - mousePos).magnitude > 0.5f){
+				StartCoroutine(Pan ());						
+				yield break;
+			} 
+
+
+
+			yield return null;
+		}
 	}
+
+	IEnumerator MobileZoom(){
+		float StartTouchDistance = (Input.touches[0].position - Input.touches[1].position).magnitude;
+		float LastTouchDistance = StartTouchDistance;
+		float CurrentDeltaDistance = 0;
+		float CurrentTouchDistance = 0;
+
+		while ( Input.touches.Length > 1 ){
+			CurrentTouchDistance =  (Input.touches[0].position - Input.touches[1].position).magnitude;
+			CurrentDeltaDistance = ( CurrentTouchDistance - LastTouchDistance );
+
+			ZoomBy( CurrentDeltaDistance );
+		}
+	}
+
+	IEnumerator ListenToZoomInput(){
+
 	
-	// Update is called once per frame
+		//Debug.Log("startZoom");
+
+
+		while (HasZoomInput()  && inputEnabled){
+
+			if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+				CurrentTouchDistance =  Input.GetAxis("Mouse ScrollWheel") * -50;
+			} else {
+				
+			}
+
+
+
+			//			Debug.Log(CurrentDeltaDistance);
+
+			LastTouchDistance =  CurrentTouchDistance;
+
+			if(CurrentDeltaDistance != 0)
+				
+			yield return null;
+
+		}
+
+		StartCoroutine( DelayedStopZoom() );
+		yield break;		
+	}
+
 	void Update () {
-	
+
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			StartCoroutine(Rotate(-45));
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			StartCoroutine(Rotate(45));
+		}
+
+		if ( !CameraAction && ( Input.GetMouseButtonDown(0) || Input.touchCount > 0 || HasZoomInput() ) )
+		{
+			StartCoroutine(CamInputControl());
+		}
+
+		if(!CameraAction && smoothMove.magnitude > 0){
+			transform.Translate(-smoothMove, Space.World);
+			smoothMove = Vector3.Lerp(smoothMove, Vector3.zero, Time.deltaTime* 10 );
+		}
+
+		if(GetZoomDelta() != 0){
+			ZoomCameraByDist( - GetZoomDelta() * Time.deltaTime * 5);
+		}
 	}
 }
--- Assets/AwesomeCamera.cs
+++ Assets/AwesomeCamera.cs
@@ -3,13 +3,135 @@ using System.Collections;
 
 public class AwesomeCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	float CameraDistanceToPlane()
+	{
+		return (MyMath.GetPlaneIntersectionY(new Ray(transform.position, transform.forward)) - transform.position).magnitude;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void SetCameraStart(){
+		currentZoomDistance = CameraDistanceToPlane();
 	}
+
+}
+
+class CameraAction : MonoBehaviour {
+	
+	public bool InProgress;
+
+	void Update(){
+
+	}
+}
+
+class CameraAction_Move : CameraAction {
+
+	IEnumerator DelayedStopDrag(){
+		for(int i = 0; i < StopDragDelay;i++) yield return new WaitForEndOfFrame();
+
+		drag = false;
+	}
+
+	IEnumerator DelayedStopZoom(){
+		for(int i = 0; i < StopDragDelay;i++) yield return new WaitForEndOfFrame();
+
+		zooming = false;
+	}
+
+	IEnumerator MoveToWorldPosition(Vector3 pos, float speed)
+	{
+
+		pos.y = 0;
+		yield return null;
+		Vector3 delta = pos - MyMath.GetCameraCenter() ;
+
+		while (delta.magnitude > 0.1f)
+		{
+			transform.Translate((pos - MyMath.GetCameraCenter()) * speed * Time.deltaTime, Space.World);
+
+			delta = pos - MyMath.GetCameraCenter();
+
+			Debug.DrawLine(MyMath.GetCameraCenter(), pos, Color.red);
+			yield return null;
+		}
+	}
+
+	public void PanToPos(Vector3 pos)
+	{
+		StartCoroutine(PanToWorldPos(pos,5));
+	}
+
+	IEnumerator Pan(){
+		startDragPos = MyMath.GetInputPos();
+		while( inputEnabled && (Input.touchCount == 1 || Input.GetMouseButton(0))){
+
+			Vector3 mousePos = MyMath.GetInputPos();
+			Vector3 delta = mousePos - startDragPos;
+			smoothMove = delta * Time.deltaTime * 10;
+			transform.position =  transform.position - smoothMove;
+			yield return null;
+		}
+
+		StartCoroutine(DelayedStopDrag());
+
+
+		if(inputEnabled && (Input.touchCount == 2)){
+			smoothMove *= 0.2f;			
+			StartCoroutine(ListenToZoomInput());	
+		}		                                   
+
+		yield break;
+	}
+
+
+}
+
+class CameraAction_Rotate : CameraAction {
+	IEnumerator Rotate(float step)
+	{
+
+		float rotated = 0;
+		float t = 0;
+
+		while (Mathf.Abs(rotated) < Mathf.Abs( step)) {
+
+			float target_rot = Mathf.Lerp(0, step, t);
+			float dr = target_rot - rotated;
+			rotated += dr;
+			t += Time.deltaTime * 3;
+			transform.RotateAround(MyMath.GetCameraCenter(), Vector3.up, dr);
+			yield return null;
+		}
+
+		transform.RotateAround(MyMath.GetCameraCenter(), Vector3.up, rotated- step);
+
+	}
+}
+
+class CameraAction_Zoom : CameraAction {
+
+	float currentZoomDistance;
+
+	void SetZoomDistance(float dist){
+		currentZoomDistance = Mathf.Clamp(dist, 1, 100);;
+	}
+
+	void ZoomCameraByDist(float dist)
+	{
+		//		Debug.Log("Zoom "+dist);
+		Vector3 newPos = transform.transform.position + (transform.transform.forward * dist);
+		transform.transform.position = newPos;
+
+	}
+
+
+	float GetDesiredCameraDistance()
+	{
+		return currentZoomDistance;
+	}
+
+	float GetZoomDelta()
+	{
+		return GetDesiredCameraDistance() - CameraDistanceToPlane();
+	}
+
 }
