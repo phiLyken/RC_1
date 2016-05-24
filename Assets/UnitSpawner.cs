f286f94bebb116291354164b81835c41b00b7bf1
phil <philipp.zupke@wooga.net>
2016-05-24 20:42:17 +0200

    merge fix

--- Assets/UnitSpawner.cs
+++ Assets/UnitSpawner.cs
@@ -4,50 +4,38 @@ using System.Linq;
 
 public class UnitSpawner : MonoBehaviour {
 
-    public bool SpawnOnAwake;
-
-    public string[] Unit_Ids;
-    public int group;
-    public MyMath.R_Range TurnTimeOnSpawn;
-
-    Color[] group_color =
-    {
-        Color.grey,
-        Color.blue,
-        Color.cyan,
-        Color.green,
-        Color.yellow
-    };
+	public int SpawnerGroupID;
 
 
-    public void SpawnUnit()
-    {
-        Unit u = UnitFactory.CreateUnit(UnitConfigsDatabase.GetConfig(MyMath.GetRandomObject(Unit_Ids.ToList())), group, (int) TurnTimeOnSpawn.Value());
-       // Debug.Log(gameObject.name+" spawning " + u.GetID());
-      
-
-        if(u != null) { 
-            UnitFactory.SpawnUnit(u, GetComponent<Tile>());
-        } else
-        {
-            Debug.LogWarning("Could not spawn unit");
-        }
-    }
-
-#if UNITY_EDITOR
-    void OnDrawGizmos()
-    {
-        if (SpawnOnAwake) { 
-            Gizmos.color = Color.red;
-            Gizmos.DrawCube(transform.position, Vector3.one*0.25f);
-        }
-
-        Gizmos.color = group_color[Mathf.Clamp(group, 0, group_color.Length - 1)] ;
-        Gizmos.DrawWireCube(transform.position , Vector3.one);
+	Color[] group_color =
+	{
+		Color.grey,
+		Color.blue,
+		Color.cyan,
+		Color.green,
+		Color.yellow
+	};
 
 
-        MyMath.SceneViewText(MyMath.StringArrToLines(Unit_Ids), transform.position + Vector3.up, Color.magenta);
-    }
+	public void SpawnUnit(ScriptableUnitConfig unit_config, int turnTime, int group)
+	{
+		Unit u = UnitFactory.CreateUnit(unit_config, group, turnTime);
+		// Debug.Log(gameObject.name+" spawning " + u.GetID());
 
-#endif
+
+		if(u != null) { 
+			UnitFactory.SpawnUnit(u, GetComponent<Tile>());
+		} else
+		{
+			Debug.LogWarning("Could not spawn unit");
+		}
+	}
+	void OnDrawGizmos()
+	{
+
+		Gizmos.color = group_color[Mathf.Clamp(SpawnerGroupID, 0, group_color.Length - 1)] ;
+		Gizmos.DrawWireCube(transform.position , Vector3.one);
+
+		//MyMath.SceneViewText(MyMath.StringArrToLines(Unit_Ids), transform.position + Vector3.up, Color.magenta);
+	}
 }
