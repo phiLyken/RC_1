65d975b1c03bbbe232887cdbebcf5475cbf719be
philipp zupke <trash@bytestyles.net>
2016-06-05 16:16:37 +0200

    added unit test spawner to test unit spawning. duh

---
+++ Assets/TileProp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TileProperties
+{
+    BlockWalkable,
+    BlockSight
+}
+public class TileProp : MonoBehaviour {
+    public List<TileProperties> Tags;
+    
+    void OnEnable()
+    {
+        Collider col = GetComponent<Collider>();
+        if (col != null) col.enabled = false;
+    }
+}
---
+++ Assets/UnitTestSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitTestSpawner : MonoBehaviour {
+
+    public ScriptableUnitConfig Config;
+
+    void Start()
+    {
+        if(Config != null)
+        {
+            UnitSpawner spawner = gameObject.AddComponent<UnitSpawner>();
+            spawner.SpawnUnit(Config, 0, 0);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if(Config != null)
+          MyMath.SceneViewText(Config.ID, transform.position);
+    }
+}
