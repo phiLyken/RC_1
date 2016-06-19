856aa2f3a79687b6d857b1be52f7323dc86cff0a
philipp zupke <trash@bytestyles.net>
2016-06-19 15:41:50 +0200

    new group spawning logic

--- Assets/Scripts/UnitSpawnManager.cs
+++ Assets/Scripts/UnitSpawnManager.cs
@@ -14,28 +14,78 @@ public class UnitSpawnManager : MonoBehaviour {
     {        
         Spawners = ( GetComponentsInChildren<UnitSpawner>()).ToList();        
     }
+    public List<int> GetSpawnersIDs(List<UnitSpawner> Spawners)
+    {
+        List<int> ids = new List<int>();
 
+        //Add distinct id's
+       foreach(UnitSpawner sp in Spawners)
+        {
+            if (!ids.Contains(sp.SpawnerGroupID))
+            {
+                ids.Add(sp.SpawnerGroupID);
+            }
+        }
+
+        return ids;
+
+    }
+
+    List<UnitSpawner> GetSpawnerForGroup(List<int> unique_ids, UnitSpawnGroupConfig group)
+    {
+        //if there are no distinct spawner ids left, stop
+        if (unique_ids.Count == 0)
+        {
+            Debug.LogWarning("Not Enough SpawnGroups in Tileset for GroupCount");
+            return null;
+        }
+        int id = group.SpawnerGroup == 0 ? MyMath.GetRandomObject(unique_ids) : group.SpawnerGroup;
+        unique_ids.Remove(id);
+
+       return Spawners.Where(sp => sp.SpawnerGroupID == id).ToList();
+    }
+   
     public void SpawnGroups(List<UnitSpawnGroupConfig> groups)
     {
-        foreach(UnitSpawnGroupConfig group in groups) {
-            List<WeightedUnit> unitconfigs = RegionLoader.GetUnitsForGroupPower(group);
-            List<UnitSpawner> spawnersForId = Spawners.Where(sp => sp.SpawnerGroupID == group.SpawnerGroup).ToList();
+
+        //first spawn groups with fixed spawner ids
+        // -> sort descending by spawner ID, hence "0" groups will be last
+        // get a list of all spawnergroups for an id
+        // remove a spawner when it has been chosen
+        // then spawn the groups without spawner and choose randomly from the remaining spawners until no more spawners
+        // or no more groups are left
+      
+        groups = groups.OrderByDescending(gr => gr.SpawnerGroup).ToList();
+
+        List<int> spawnerIDs = GetSpawnersIDs(Spawners);
+
+        foreach (UnitSpawnGroupConfig group in groups) {
+
+
             globalGroupCounter++;
 
-            if(unitconfigs.Count > spawnersForId.Count)
+            List<UnitSpawner> spawnersForGroup = GetSpawnerForGroup(spawnerIDs, group);
+            if(spawnersForGroup == null || spawnersForGroup.Count == 0)
             {
-                Debug.LogWarning("Not enough spawners for my group :(   spawners:" + spawnersForId.Count+"  units:"+ unitconfigs.Count);
                 return;
             }
 
-            while(spawnersForId.Count > 0 && unitconfigs.Count > 0)
+            List<WeightedUnit> unitConfigs = RegionLoader.GetUnitsForGroupPower(group);
+
+            if (unitConfigs.Count > spawnersForGroup.Count)
+            {
+                Debug.LogWarning("Not enough spawners for my group :(   spawners:" + spawnersForGroup.Count+"  units:"+ unitConfigs.Count);
+                return;
+            }
+
+            while(spawnersForGroup.Count > 0 && unitConfigs.Count > 0)
             {
                 
-                WeightedUnit unit = unitconfigs[Random.Range(0, unitconfigs.Count)];
-                UnitSpawner spawner = spawnersForId[Random.Range(0, spawnersForId.Count)];
+                WeightedUnit unit = unitConfigs[Random.Range(0, unitConfigs.Count)];
+                UnitSpawner spawner = spawnersForGroup[Random.Range(0, spawnersForGroup.Count)];
 
-                unitconfigs.Remove(unit);
-                spawnersForId.Remove(spawner);
+                unitConfigs.Remove(unit);
+                spawnersForGroup.Remove(spawner);
 
                 spawner.SpawnUnit( unit.UnitConfig, (int)unit.TurnTimeOnSpawn.Value(), globalGroupCounter);
             }
