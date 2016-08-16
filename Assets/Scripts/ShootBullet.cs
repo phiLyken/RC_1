2e8c78acd650eb9cf746617ceb84e03e9e55c265
philipp zupke <trash@bytestyles.net>
2016-08-16 22:46:37 +0200

    added simple particles

--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -7,9 +7,10 @@ public class ShootBullet : MonoBehaviour
     public int bullet_count;
     public float intervall;
     public BulletConfig Bullet;
-    public GameObject ShootEffectOnWeapon;
-    public GameObject HitEffect;
+    public EffectSpawner ShootEffectOnWeapon;
+    public EffectSpawner HitEffect;
 
+    
     public Sequence Shoot(Transform target)
     {
         return ShootBullets(target);
@@ -39,7 +40,73 @@ public class ShootBullet : MonoBehaviour
 
     Sequence ShootOneBullet(Transform target)
     {
+        if (ShootEffectOnWeapon != null)
+            ShootEffectOnWeapon.Init(transform.gameObject);
+
         return BulletEmitter.SpawnBullet(Bullet, gameObject.transform, target);
         
     }
+}
+
+[System.Serializable]
+public class EffectSpawner{
+
+    public AudioClip Sound;
+    public ParticleSystem Particle;
+    public LightFlash Flash;
+
+    public void Init(GameObject target)
+    {
+        if(Sound != null)
+        {
+
+        }
+
+        if(Particle != null)
+        {
+            GameObject go = GameObject.Instantiate(Particle, target.transform.position, target.transform.rotation) as GameObject;
+  
+
+        }
+
+        if(Flash != null)
+        {
+            Flash.Init(target);
+        }
+    }
+}
+
+[System.Serializable]
+public class LightFlash
+{
+    public float base_intensity;
+    public float radius;
+    public Color startColor;
+    public Color endColor;
+    public AnimationCurve intensity;
+    public float duration;
+
+    public static void Init(LightFlash flash, GameObject target)
+    {
+        GameObject new_go = new GameObject("_flash");
+        new_go.transform.position = target.transform.position;
+        
+        Light l =  new_go.AddComponent<Light>();
+        l.range = flash.radius;
+        l.intensity = flash.base_intensity;
+
+        Sequence FlashSequence = DOTween.Sequence();
+        FlashSequence.Append(    DOTween.To(() => l.intensity, _l => l.intensity = _l,0, flash.duration).SetEase(flash.intensity));
+        FlashSequence.AppendCallback(() => GameObject.Destroy(new_go));
+
+        l.color = flash.startColor;
+        l.DOColor(flash.endColor, flash.duration);
+      
+    }
+
+    public void Init(GameObject target)
+    {
+        Init(this, target);
+    }
+   
 }
