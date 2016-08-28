475562b71d74f1f0255b0a1457efce0780e99b68
philipp zupke <trash@bytestyles.net>
2016-08-28 15:59:41 +0200

    turn list with moving items

--- Assets/Scripts/UI_TurnList.cs
+++ Assets/Scripts/UI_TurnList.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using System;
-
+using DG.Tweening;
 public class UI_TurnList : MonoBehaviour {
 
     public int TurnListEntries;
 
     ViewList<ITurn, UI_TurnListItem> views;
+    Dictionary<UI_TurnListItem, int> positions;
+    Dictionary<RectTransform,Sequence> move_sequences;
 
     public TurnSystem m_turnsystem;
 
@@ -25,6 +27,8 @@ public class UI_TurnList : MonoBehaviour {
     {
         CreateAnchors();
         views = new ViewList<ITurn, UI_TurnListItem>();
+        positions = new Dictionary<UI_TurnListItem, int>();
+        move_sequences = new Dictionary<RectTransform, Sequence>();
         views.Init(MakeItem);
         
         if(turn_system != null)
@@ -37,9 +41,9 @@ public class UI_TurnList : MonoBehaviour {
     {
         ViewsUpdated(views.UpdateList(items));
     }
+
     UI_TurnListItem MakeItem(ITurn u)
-    {
-        Debug.Log(u.GetColor());
+    {     
         UI_TurnListItem new_item = Instantiate(TurnListItemPrefab.gameObject).GetComponent<UI_TurnListItem>();
         new_item.transform.SetParent(this.transform,false); 
         new_item.SetTurnItem(u);
@@ -50,7 +54,7 @@ public class UI_TurnList : MonoBehaviour {
     void CreateAnchors()
     {
         anchors = MyMath.SpawnObjectsToTransform<RectTransform>(TurnListAnchorPrefab, parent_container, TurnListEntries);
-        Debug.Log("ANCHORS " + anchors.Count);
+       
         Canvas.ForceUpdateCanvases();
     }
 
@@ -59,24 +63,96 @@ public class UI_TurnList : MonoBehaviour {
         int i = 0;
         foreach(var pair in list)
         {
-            if(i >= TurnListEntries)
-            {
-                pair.Value.gameObject.SetActive(false);
-                continue;
-            }
-            Debug.Log(GetTargetSlotPos(i));
-
-            pair.Value.gameObject.SetActive(true);
-            pair.Value.GetComponent<RectTransform>().position = GetTargetSlotPos(i);
-
+            UpdateSlotPosition(pair.Key, pair.Value, i);           
             i++;
         }
+    }
 
+    void UpdateSlotPosition(ITurn turn, UI_TurnListItem view, int position_in_list)
+    {
+
+        if (position_in_list >= TurnListEntries)
+        {
+            view.gameObject.SetActive(false);
+            return;
+        }
+
+        view.gameObject.SetActive(true);
+
+        int from_slot = 0;
+
+        if (!positions.ContainsKey(view))
+        {
+            positions.Add(view, position_in_list);
+        }
+        else
+        {
+            from_slot = positions[view];
+            positions[view] = position_in_list;
+        }
+
+         MoveView(view, position_in_list, from_slot);
+        
+    }
+
+    void MoveView(UI_TurnListItem view, int slot_id_to, int slot_id_from )
+    {
+        view.TF.text = slot_id_to.ToString();
+        RectTransform target = view.GetComponent<RectTransform>();
+        StopMove(target);
+        if (  Math.Abs(slot_id_to - slot_id_from) > 1 )
+        {
+            LongSlotMove(target, slot_id_from, slot_id_to);
+        } else
+        {
+            ShortSlotMove(target, slot_id_to);
+        }
     }
 
     Vector3 GetTargetSlotPos(int pos)
     {
-        Debug.Log(pos +" "+anchors.Count);
+      //  Debug.Log(pos +" "+anchors.Count);
         return anchors[pos].position;
+    }
+
+    void StopMove(RectTransform view)
+    {
+        if (move_sequences.ContainsKey(view))
+        {
+            move_sequences[view].Kill();
+            move_sequences.Remove(view);
+        }
+    }
+
+    Sequence LongSlotMove(RectTransform view, int start_slot, int target_slot_id)
+    {
+        Vector3 first_pos = GetTargetSlotPos(start_slot) - new Vector3(0, 50,0);
+        Vector3 second_pos = GetTargetSlotPos(target_slot_id) - new Vector3(0, 50, 0);
+        Vector3 final_pos = GetTargetSlotPos(target_slot_id);
+         
+        Sequence move = DOTween.Sequence();
+        
+        move.Append(view.DOMove(first_pos, 0.5f));
+        move.Append(view.DOMove(second_pos, 1f));
+        move.Append(view.DOMove(final_pos, 0.5f));
+        move.AppendCallback(() => move_sequences.Remove(view));
+
+        move_sequences.Add(view, move);
+
+        return null;
+    }
+
+    Sequence ShortSlotMove(RectTransform view, int slot_id)
+    {     
+        RectTransform rect = view.GetComponent<RectTransform>();
+        Vector3 target_position = GetTargetSlotPos(slot_id);
+        Sequence move = DOTween.Sequence();
+
+        move.Append(rect.DOMove(target_position, 0.4f));
+        move.AppendCallback(() => move_sequences.Remove(view));
+
+        move_sequences.Add(view, move);
+
+        return move;
     }
 }
