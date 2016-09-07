e8e492b5939aad7cd17d6692103deaf27df8da89
Matthias Zündorf <Matthias Zndorf>
2016-09-07 04:59:41 +0200

    added variant textures for prisoners, alternate faces and finished materials on suit02

---
+++ Assets/Scripts/RandomMeshSelector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+
+public class RandomMeshSelector : MonoBehaviour {
+       
+	public GameObject[] targetObject;
+	public GameObject[] randomObject;
+
+    // Use this for initialization
+    void Start()
+    {
+		foreach (GameObject target in targetObject)
+		{ 
+			GameObject newObject = Instantiate(randomObject[Random.Range(0, randomObject.Length)]) as GameObject;
+			newObject.transform.parent = target.transform;
+
+		}
+			
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
+
+//targetList[0]
