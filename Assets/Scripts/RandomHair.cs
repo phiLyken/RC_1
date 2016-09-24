863c6ac46469878e15441b1a7a0e364e695beaec
Matthias Zündorf <Matthias Zndorf>
2016-09-24 20:48:44 +0200

    added bodyglove with textures

---
+++ Assets/Scripts/RandomHair.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(SkinnedMeshRenderer))]
+
+public class RandomHair : MonoBehaviour {
+       
+	//public GameObject[] targetObject;
+    public GameObject[] randomObject;
+    public GameObject headbone;
+    // Use this for initialization
+    void Start()
+    {
+     
+     //   foreach (GameObject target in targetObject)
+	//	{
+
+            GameObject newhair = Instantiate(randomObject[Random.Range(0, randomObject.Length)]) as GameObject;
+
+            newhair.GetComponent<Transform>().position = headbone.GetComponent<Transform>().position;
+            newhair.transform.parent = headbone.transform;
+ 
+      //  }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
+
+//targetList[0]
--- Assets/Scripts/RandomMeshSelector.cs
+++ Assets/Scripts/RandomMeshSelector.cs
@@ -1,23 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(SkinnedMeshRenderer))]
+//[RequireComponent(typeof(SkinnedMeshRenderer))]
 
 public class RandomMeshSelector : MonoBehaviour {
        
 	public GameObject[] targetObject;
-	public GameObject[] randomObject;
-
+    public GameObject[] randomObject;
+    //public Mesh[] randomObject;
+    public GameObject headbone;
     // Use this for initialization
     void Start()
     {
-		foreach (GameObject target in targetObject)
-		{ 
-			GameObject newObject = Instantiate(randomObject[Random.Range(0, randomObject.Length)]) as GameObject;
-			newObject.transform.parent = target.transform;
+     
+        foreach (GameObject target in targetObject)
+		{
 
-		}
-			
+      //      headbone = target.transform.Find("humanoid/humanoid Pelvis/humanoid Spine/humanoid Spine1/humanoid Neck/humanoid Head").gameObject;
+
+
+
+
+            //GameObject newObject = Instantiate(randomObject[Random.Range(0, randomObject.Length)]) as GameObject;
+            GameObject blubb = Instantiate(randomObject[Random.Range(0, randomObject.Length)]) as GameObject;
+
+            blubb.GetComponent<Transform>().position = headbone.GetComponent<Transform>().position;
+            blubb.transform.parent = headbone.transform;
+            // target.GetComponent<SkinnedMeshRenderer>().sharedMesh = randomObject[Random.Range(0, randomObject.Length)];
+            //   newObject.transform.parent = target.transform;
+
+        }
+
     }
 
     // Update is called once per frame
