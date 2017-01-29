8801c0df352842c52242cc1a14f6b4ac8ee2b762
philipp zupke <trash@bytestyles.net>
2017-01-30 00:27:22 +0100

    removed redudant canvas from tutorial addedstoppable routines

---
+++ Assets/External/Generic/RoutineHandler/Example.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Example of using the StoppableCoroutine wrapper.
+/// </summary>
+public class Example : MonoBehaviour
+{
+    // MonoBehaviour.Start
+    protected IEnumerator Start()
+    {
+       
+        // Create the stoppable coroutine and store it
+        var routine = MyCoroutine().MakeStoppable();
+        // Pass the wrapper to StartCoroutine
+        StartCoroutine(routine);
+
+        var routine2 = RoutineWithParam("OMG", delegate
+        { return !Input.GetKey(KeyCode.A); }).MakeStoppable(() => Debug.Log("HAAHHA"));
+
+        StartCoroutine(routine2);
+
+        // Do stuff...
+        yield return new WaitForSeconds(4);
+
+        // Abort the coroutine by calling Stop() on the wrapper
+        routine.Stop();
+        routine2.Stop();
+    }
+
+    // Coroutine that runs indefinitely and can only
+    // be stopped from the outside
+    protected IEnumerator MyCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Debug.Log("running...");
+        }
+    }
+
+    protected IEnumerator RoutineWithParam(string foo, Func<bool> canExecute)
+    {
+        while (canExecute != null && canExecute() )
+        {
+            Debug.Log("running too");
+            yield return null;
+        }
+
+        Debug.Log(foo);
+    }
+}
---
+++ Assets/External/Generic/RoutineHandler/StoppableCoroutine.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Wrapper around coroutines that allows to start them without using
+/// their lexical name while still being able to stop them.
+/// https://gist.github.com/sttz/4578868#file-stoppablecoroutine-cs
+/// </summary>
+/// 
+
+public class StoppableCoroutine : IEnumerator
+{
+    // Wrapped generator method
+    protected IEnumerator generator;
+
+    Action callback;
+
+    public StoppableCoroutine(IEnumerator generator, Action _callback)
+    {
+        this.callback = _callback;
+        this.generator = generator;
+    }
+
+    public StoppableCoroutine(IEnumerator generator)
+    {
+        this.generator = generator;
+    }
+
+    // Stop the coroutine form being called again
+    public void Stop()
+    {
+        generator = null;
+        callback.AttemptCall();
+        callback = null;
+    }
+
+    // IEnumerator.MoveNext
+    public bool MoveNext()
+    {
+        if (generator != null)
+        {
+            bool hasNext = generator.MoveNext();
+            if (!hasNext)
+            {
+                callback.AttemptCall();
+                callback = null;
+            }
+            return hasNext;
+        }
+        else
+        {
+            callback.AttemptCall();
+            callback = null;
+            return false;
+        }
+    }
+
+    // IEnumerator.Reset
+    public void Reset()
+    {
+        if (generator != null)
+        {
+            generator.Reset();
+            callback.AttemptCall();
+            callback = null;
+        }
+    }
+
+    // IEnumerator.Current
+    public object Current
+    {
+        get
+        {
+            if (generator != null)
+            {
+                return generator.Current;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Syntactic sugar to create stoppable coroutines.
+/// </summary>
+public static class StoppableCoroutineExtensions
+{
+    public static StoppableCoroutine MakeStoppable(this IEnumerator generator)
+    {
+        return new StoppableCoroutine(generator);
+    }
+
+    public static StoppableCoroutine MakeStoppable(this IEnumerator generator, Action callback)
+    {
+        return new StoppableCoroutine(generator, callback);
+    }
+}
+ 
