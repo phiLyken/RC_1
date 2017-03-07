18407d249470da92d7b6b4d06226afd20f7963b8
Semjon Leinweber <semjon.leinweber@innogames.com>
2017-03-07 03:54:29 +0100

    *loot balancing pass

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -117,7 +117,7 @@ public class Constants : MonoBehaviour {
     /// <returns></returns>
     public static int GetDustForProgress(int base_amount, int regionDifficulty)
     {
-        return base_amount * regionDifficulty;
+        return base_amount * (int)Mathf.Pow(2,regionDifficulty-1);
     }
 
     /// <summary>
