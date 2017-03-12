d9c2982254d4b9d29918b60b2697608884318c28
philipp zupke <trash@bytestyles.net>
2017-03-12 01:18:14 +0100

    kongregate api calls

---
+++ Assets/KongregateAPIBehaviour.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KongregateAPIBehaviour : MonoBehaviour
+{
+    private static KongregateAPIBehaviour instance;
+
+
+    string username;
+    string gameAuthToken;
+    int userId;
+
+    void Start()
+    {
+       
+        if (instance == null)
+        {
+            if (Application.isEditor)
+            {
+                OnKongregateUserInfo("123|TEST|AUTH");
+            }
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Object.DontDestroyOnLoad(gameObject);
+        gameObject.name = "KongregateAPI";
+
+        Application.ExternalEval(
+          @"if(typeof(kongregateUnitySupport) != 'undefined'){
+        kongregateUnitySupport.initAPI('KongregateAPI', 'OnKongregateAPILoaded');
+      };"
+        );
+
+
+    
+ 
+    }
+
+    public void OnKongregateAPILoaded(string userInfoString)
+    {
+        OnKongregateUserInfo(userInfoString);
+    }
+
+    public void OnKongregateUserInfo(string userInfoString)
+    {
+        var info = userInfoString.Split('|');
+        userId = System.Convert.ToInt32(info[0]);
+        username = info[1];
+        gameAuthToken = info[2];
+        Debug.Log("Kongregate User Info: " + username + ", userId: " + userId);
+
+        Unit.OnUnitKilled += OnKill;
+        PlayerLevel.Instance.OnLevelUp += OnLevelUp;
+        MissionOutcome.OnMissionOutcomeSet += OnOutcome;
+       
+    }
+    
+    void OnOutcome(MissionOutcome outcome)
+    {
+       
+
+       var Regions = new List<RegionConfigDataBase>(Resources.Load<ScriptableRegionDataBaseConfigs>("Regions/RegionConfigs/selectablemissions_defaultbalancing").RegionConfigs);
+
+        int saved = 0;
+        foreach(var region in Regions)
+        {
+            saved += region.IsCompleteInSave() ? 1 : 0;
+        }
+
+        Application.ExternalCall("kongregate.stats.submit", "SECURED SUPPLIES", outcome.SuppliesGainedFinal);
+        Application.ExternalCall("kongregate.stats.submit", "MISSIONS CLEARED", saved);
+        if(saved >= Regions.Count)
+        {
+            Application.ExternalCall("kongregate.stats.submit", "GameComplete 1", 1);
+        }
+    }
+
+    void OnLevelUp(int obj)
+    {
+        Application.ExternalCall("kongregate.stats.submit", "PLAYER LEVEL", obj);
+    }
+
+ 
+    void OnKill(Unit u)
+    {
+        if(u.OwnerID == 1)
+        {
+            Application.ExternalCall("kongregate.stats.submit", "PRISONERS PACIFIED", 1);
+        }
+    }
+}
