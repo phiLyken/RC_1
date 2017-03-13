c53995d638f77d3552474c38adadd7e01120ece9
philipp zupke <trash@bytestyles.net>
2017-03-14 00:02:47 +0100

    Improved squadselection loading

--- Assets/UI_Menu.cs
+++ Assets/UI_Menu.cs
@@ -4,17 +4,29 @@ using DG.Tweening;
 using UnityEngine.UI;
 public class UI_Menu : MonoBehaviour {
 
-    public GameObject SquadSelection;
+    public GameObject SquadSelectionPrefab;
+
+    GameObject SquadSelection;
     public GameObject Title;
     public GameObject MainMenu;
     public GameObject TitleAnchorTop;
     public GameObject TitleAnchorBottom;
 
+    
     public void ShowSquadSelection(bool show)
     {
         MainMenu.SetActive(!show);
-        SquadSelection.SetActive(show);
 
+        if(!show && SquadSelection == null)
+        {
+            return;
+        }
+        if(SquadSelection == null)
+        {
+            SquadSelection = Instantiate(SquadSelectionPrefab) as GameObject;       
+        }
+        Debug.Log("FOO");
+        SquadSelection.SetActive(show);
         Sequence move = DOTween.Sequence();
         Title.transform.DOMove(show ? TitleAnchorTop.transform.position : TitleAnchorBottom.transform.position, 0.3f);
         Title.GetComponentInChildren<Text>().text = show ? "MISSION BRIEFING" : "REDCLIFF";
